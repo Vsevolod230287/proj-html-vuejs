d99747281e612c26d0a789eca74408597dd0aee8
Vsevolod <cerneavschivsevolod@gmail.com>
2021-04-21 18:01:53 +0200

    Jumbo 25%

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,8 +2,9 @@
 var app = new Vue({
   el: '#root',
   data: {
+    index: 0,
+    langueges: ['en', 'fr', 'de'],
     headerLinks: ['Home', 'Pages', 'Courses', 'Features', 'Blog', 'Shop'],
-
   },
   methods: {},
 })
