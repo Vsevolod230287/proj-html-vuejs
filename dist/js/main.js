bf338d1b87940f3513554ef9c8ee5287f7931720
Vsevolod <cerneavschivsevolod@gmail.com>
2021-04-21 21:54:12 +0200

    Aggiustamenti header e jumbo

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,10 +1,61 @@
-
 var app = new Vue({
   el: '#root',
   data: {
+    selected: '',
     index: 0,
-    langueges: ['en', 'fr', 'de'],
-    headerLinks: ['Home', 'Pages', 'Courses', 'Features', 'Blog', 'Shop'],
+    langueges: [{
+        shortLang: 'en',
+        fullLang: 'English',
+        langPath: './dist/business/images/en.png'
+      },
+      {
+        shortLang: 'fr',
+        fullLang: 'French',
+        langPath: './dist/business/images/fr.png'
+
+      },
+      {
+        shortLang: 'de',
+        fullLang: 'Deutsch',
+        langPath: './dist/business/images/fr.png'
+
+      }
+    ],
+    headerLinks: [{
+        name: 'Home',
+        url: '#',
+      },
+      {
+        name: 'Pages',
+        url: '#',
+      },
+      {
+        name: 'Courses',
+        url: '#',
+      },
+      {
+        name: 'Features',
+        url: '#',
+      },
+      {
+        name: 'Blog',
+        url: '#',
+      },
+      {
+        name: 'Shop',
+        url: '#',
+      },
+
+    ],
   },
-  methods: {},
+  methods: {
+  getlangPath: function() {
+    this.langueges.forEach((languege) => {
+      if(this.selected == languege.fullLang){
+        console.log(languege.langPath);
+      }
+    });
+
+    }
+  },
 })
--- webpack.mix.js
+++ webpack.mix.js
@@ -3,3 +3,5 @@ let mix = require('laravel-mix');
 mix.sass('src/scss/master.scss','dist/css/').options({
   processCssUrls: false
 })
+
+mix.js('src/app.js', 'dist/js/');
