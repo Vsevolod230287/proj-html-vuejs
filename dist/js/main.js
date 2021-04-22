523343604444b8b8190cfba555916c4938c27d8d
Vsevolod <cerneavschivsevolod@gmail.com>
2021-04-22 17:40:53 +0200

    Iniziato Main primo slider

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,14 @@
 var app = new Vue({
   el: '#root',
   data: {
+    arrayImg: [
+      './dist/business/images/home-business-service-slide-01-480x298.jpg',
+      './dist/business/images/home-business-service-slide-02-480x298.jpg',
+      './dist/business/images/home-business-service-slide-03-480x298.jpg',
+
+
+    ],
+    id: '',
     selected: '',
     index: 0,
     langueges: [{
@@ -49,13 +57,41 @@ var app = new Vue({
     ],
   },
   methods: {
-  getlangPath: function() {
-    this.langueges.forEach((languege) => {
-      if(this.selected == languege.fullLang){
-        console.log(languege.langPath);
+    next: function(e) {
+      if (e && e.type == 'click') {
+        clearInterval(this.id);
+        this.id = '';
       }
-    });
-
+      if (this.index < this.arrayImg.length - 1) {
+        this.index++
+      } else {
+        this.index = 0;
+      }
+    },
+    prev: function(e) {
+      if (e.type == 'click') {
+        clearInterval(this.id);
+        this.id = '';
+      }
+      if (this.index > 0) {
+        this.index--
+      } else {
+        this.index = this.arrayImg.length - 1;
+      }
+    },
+    dotClick: function(key) {
+      this.index = key;
+    },
+    autoplay: function() {
+      if (!this.id) {
+        this.id = setInterval(() => {
+          this.next();
+        }, 1300)
+      } else {
+        clearInterval(this.id)
+        this.id = '';
+      }
     }
+
   },
 })
--- webpack.mix.js
+++ webpack.mix.js
@@ -3,5 +3,3 @@ let mix = require('laravel-mix');
 mix.sass('src/scss/master.scss','dist/css/').options({
   processCssUrls: false
 })
-
-mix.js('src/app.js', 'dist/js/');
