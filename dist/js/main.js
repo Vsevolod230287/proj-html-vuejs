a233e8c24a38f7917628ecf719af93a4ad27e9e8
Vsevolod <cerneavschivsevolod@gmail.com>
2021-04-22 19:04:05 +0200

    Passo le Informazioni delle card-carousel in modo dinamico con vue

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,16 +1,28 @@
 var app = new Vue({
   el: '#root',
   data: {
+    carouselCards: [{
+        h2: 'Consultative Training',
+        p: 'With a coach addressing multiple issues that are off balance, affecting your business in various unconsidered ways.',
+      },
+      {
+        h2: 'Real Deal Coaching',
+        p: 'Brings an exceptionally powerful opportunity. Being able to accept, to work with that opportunity.',
+      },
+      {
+        h2: 'Advisor Training Program',
+        p: 'Getting high quality, entrepreneur mindset driven online business coaching, is what is needed.',
+      }
+    ],
     arrayImg: [
       './dist/business/images/home-business-service-slide-01-480x298.jpg',
       './dist/business/images/home-business-service-slide-02-480x298.jpg',
       './dist/business/images/home-business-service-slide-03-480x298.jpg',
-
-
     ],
     id: '',
-    selected: '',
+    selected: './dist/business/images/en.png',
     index: 0,
+
     langueges: [{
         shortLang: 'en',
         fullLang: 'English',
@@ -29,6 +41,7 @@ var app = new Vue({
 
       }
     ],
+
     headerLinks: [{
         name: 'Home',
         url: '#',
