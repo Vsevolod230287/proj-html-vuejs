4c45d9debeb36be0a18824537c84181e559ede1e
Vsevolod <cerneavschivsevolod@gmail.com>
2021-04-23 19:40:00 +0200

    Fatta la funzionalità del primo slider : al click sui puntini mi cambia tre immagini alla volta.

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,29 +1,20 @@
 var app = new Vue({
+
   el: '#root',
   data: {
-    carouselCards: [{
-        h2: 'Consultative Training',
-        p: 'With a coach addressing multiple issues that are off balance, affecting your business in various unconsidered ways.',
-      },
-      {
-        h2: 'Real Deal Coaching',
-        p: 'Brings an exceptionally powerful opportunity. Being able to accept, to work with that opportunity.',
-      },
-      {
-        h2: 'Advisor Training Program',
-        p: 'Getting high quality, entrepreneur mindset driven online business coaching, is what is needed.',
-      }
-    ],
-    arrayImg: [
-      './dist/business/images/home-business-service-slide-01-480x298.jpg',
-      './dist/business/images/home-business-service-slide-02-480x298.jpg',
-      './dist/business/images/home-business-service-slide-03-480x298.jpg',
+
+    coursesImgs: [
+      './dist/business/images/course-02-443x600.jpg',
+      './dist/business/images/stock-full-hd-03-443x600.jpg',
+      './dist/business/images/stock-full-hd-04-443x600.jpg',
+      './dist/business/images/stock-full-hd-06-443x600.jpg'
     ],
     id: '',
     selected: './dist/business/images/en.png',
     index: 0,
 
-    langueges: [{
+    langueges: [
+      {
         shortLang: 'en',
         fullLang: 'English',
         langPath: './dist/business/images/en.png'
@@ -41,8 +32,8 @@ var app = new Vue({
 
       }
     ],
-
-    headerLinks: [{
+    headerLinks: [
+      {
         name: 'Home',
         url: '#',
       },
@@ -68,43 +59,89 @@ var app = new Vue({
       },
 
     ],
+    sliders: [
+      {
+        arrayImg: [
+          {
+            link: './dist/business/images/home-business-service-slide-01-480x298.jpg',
+            indice: 0,
+            h2: 'Consultative Training',
+            p: 'With a coach addressing multiple issues that are off balance, affecting your business in various unconsidered ways.',
+
+
+          },
+          {
+            link: './dist/business/images/home-business-service-slide-02-480x298.jpg',
+            indice: 1,
+            h2: 'Real Deal Coaching',
+            p: 'Brings an exceptionally powerful opportunity. Being able to accept, to work with that opportunity.',
+          },
+          {
+            link: './dist/business/images/home-business-service-slide-03-480x298.jpg',
+            indice: 2,
+            h2: 'Advisor Training Program',
+            p: 'Getting high quality, entrepreneur mindset driven online business coaching, is what is needed.',
+          }
+        ]
+      },
+      {
+        arrayImg: [
+          {
+            link: './dist/business/images/1.jpeg',
+            indice: 0,
+            h2: 'Representative efforts',
+            p: 'Success is not final, failure is not fatal: it is the courage to continue that counts. Play whith  rules, be ferocious.',
+
+
+          },
+          {
+            link: './dist/business/images/2.jpg',
+            indice: 1,
+            h2: 'Just do it!',
+            p: 'Business opportunities are like buses, there’s always another one coming. Without problems we would not grow.',
+          },
+          {
+            link: './dist/business/images/3.jpg',
+            indice: 2,
+            h2: 'You only have to do!',
+            p: 'And the day came when the risk to remain tight in a bud was more painful than the risk it took to blossom.',
+          }
+        ]
+      },
+      {
+        arrayImg: [
+          {
+            link: './dist/business/images/4.jpg',
+            indice: 0,
+            h2: 'Promote yourself!',
+            p: 'I owe my success to having listened respectfully to the very best advice, and then doing the exact opposite. Go soon!',
+
+
+          },
+          {
+            link: './dist/business/images/5.jpg',
+            indice: 1,
+            h2: 'The best prize!',
+            p: 'If you really look closely, most overnight successes took a long time. If you are on the right track, you’ll get run.',
+          },
+          {
+            link: './dist/business/images/6.png',
+            indice: 2,
+            h2: 'The real test!',
+            p: 'The test: when you let it harden or shame you into inaction, or when you learn from it; when you choose to persevere.',
+          }
+        ]
+      }
+    ],
   },
   methods: {
-    next: function(e) {
-      if (e && e.type == 'click') {
-        clearInterval(this.id);
-        this.id = '';
-      }
-      if (this.index < this.arrayImg.length - 1) {
-        this.index++
-      } else {
-        this.index = 0;
-      }
+    clickDot: function(indexDot) {
+      this.index = indexDot;
+
     },
-    prev: function(e) {
-      if (e.type == 'click') {
-        clearInterval(this.id);
-        this.id = '';
-      }
-      if (this.index > 0) {
-        this.index--
-      } else {
-        this.index = this.arrayImg.length - 1;
-      }
-    },
-    dotClick: function(key) {
-      this.index = key;
-    },
-    autoplay: function() {
-      if (!this.id) {
-        this.id = setInterval(() => {
-          this.next();
-        }, 1300)
-      } else {
-        clearInterval(this.id)
-        this.id = '';
-      }
-    }
+
+
 
   },
+  
 })
